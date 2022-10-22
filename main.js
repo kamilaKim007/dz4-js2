bb3b4e5fc37d291a2cbb7c196b1db6de83ba857c
Kim Kamilla <kamila258568@gmail.com>
2022-10-22 15:00:08 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+let menu = document.querySelector('.menu')
+let row = document.querySelector('.row')
+
+
+const getProducts = () => {
+    fetch(`https://fakestoreapi.com/products?limit=2`)
+    .then((res) => res.json())
+    .then((res) => {
+       res.forEach((item) => {
+           row.innerHTML += `
+           <div class = 'card'>
+           <img class = 'cardImg' src='${item.image}' alt = ''>
+           <h2 class = 'cardTitle'>${item.title}</h2>
+           <p class = 'cardSubtitle'>${item.description}</p>
+           <p class = ' cardPrice'> price: ${item.price}</p>
+           <p class = 'cardRating'> rating: ${item.rating.rate}</p>
+           </div>
+           `
+       })
+    })
+}
+getProducts('all')
+
+
+getCategories()
+fetch("https://fakestoreapi.com/products?limit=2")
+.then(response => response.json())
+.then((data) => {
+    let data1 = "";
+    data.map((values) => {
+        data1+= `
+        <div class="card">
+            <div class="image">
+                <img src=${values.image} id="poster">
+            </div>
+            <h2 id="title">${values.title}</h2>
+            <div class="price">
+                <h4 id="price">Price: $${values.price}</h4>
+                <h5 id="rating">Rating: ${values.rating.rate}</h5>
+            </div>
+        </div>
+        `
+        document.querySelector('.container').innerHTML=data1;
+    })
+})
+.catch(err => {
+	console.error(err);
+});
